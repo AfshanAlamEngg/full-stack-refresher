3c09d0dc0fdb5e8f47d7d19408a9ecadda13b364
AfshanAlamEngg <afshanalamengg@gmail.com>
2026-09-21 23:49:44 +0530

    Update: Chapter 2 code

--- 02-more-typescript/any.js
+++ 02-more-typescript/any.js
@@ -1,9 +1,16 @@
 "use strict";
-let val = 2;
-console.log(val); // Output: 2
-val = "Hello";
-console.log(val); // Output: "Hello"
+// let val: any = 2;
+// console.log(val); // Output: 2
+// val = "Hello";
+// console.log(val); // Output: "Hello"
+// val = new Array();
+// // val.push(5);
+// val.doesnotexist(33);
+// console.log(val); // Output: [5]
+let val = 22;
+val = "some string";
 val = new Array();
-// val.push(5);
-val.doesnotexist(33);
+if (val instanceof Array) {
+    val.push(5);
+}
 console.log(val); // Output: [5]
--- 02-more-typescript/any.ts
+++ 02-more-typescript/any.ts
@@ -1,8 +1,16 @@
-let val: any = 2;
-console.log(val); // Output: 2
-val = "Hello";
-console.log(val); // Output: "Hello"
+// let val: any = 2;
+// console.log(val); // Output: 2
+// val = "Hello";
+// console.log(val); // Output: "Hello"
+// val = new Array();
+// // val.push(5);
+// val.doesnotexist(33);
+// console.log(val); // Output: [5]
+
+let val: unknown = 22;
+val = "some string";
 val = new Array();
-// val.push(5);
-val.doesnotexist(33);
+if (val instanceof Array) {
+    val.push(5);
+}
 console.log(val); // Output: [5]
